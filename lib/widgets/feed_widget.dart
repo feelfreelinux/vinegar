e1c086d43c108e004a7a707847d89d6753fd987c
xdk78 <xdk78888@gmail.com>
2018-12-15 19:54:37 +0100

    feat: display images in feed

--- lib/widgets/feed_widget.dart
+++ lib/widgets/feed_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vinegar/widgets/widgets.dart';
 import 'package:vinegar/models/models.dart';
+import 'package:flutter_advanced_networkimage/flutter_advanced_networkimage.dart';
 
 class FeedWidget extends StatelessWidget {
   @override
@@ -30,12 +31,12 @@ class FeedWidget extends StatelessWidget {
         Article article = articles[index];
         return Column(
           children: <Widget>[
-            ListTile(
-              title: Text(article.title),
-            ),
-            Divider(
-              height: 2.0,
-            ),
+            Card(
+                child: ListTile(
+                    title: Text(article.title),
+                    trailing: Image(
+                        image: AdvancedNetworkImage(article.thumbnail,
+                            useDiskCache: true))))
           ],
         );
       },
