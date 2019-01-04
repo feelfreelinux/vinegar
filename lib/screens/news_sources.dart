285a86b69a1f19c849754ab7cced2931916de3d1
xdk78 <xdk78888@gmail.com>
2019-01-04 19:00:59 +0100

    feat: show modal bottom sheet in news sources

--- lib/screens/news_sources.dart
+++ lib/screens/news_sources.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:vinegar/widgets/widgets.dart';
 import 'package:vinegar/models/models.dart';
-import 'package:pref_gen_flutter/pref_gen_flutter.dart';
 import 'dart:convert';
 
 class NewsSourcesScreen extends StatelessWidget {
@@ -40,8 +38,83 @@ class NewsSourcesScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          _showAddRedditNewsSource(context);
+          showModalBottomSheet<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                padding: EdgeInsets.all(7.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(14.0),
+                      child: Text(
+                        "ADD NEW SOURCE",
+                        style: TextStyle(
+                          fontSize: 13.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        _iconAddNewButton(Icons.add_box, "REDDIT",
+                            () => _showAddRedditNewsSource(context)),
+                        _iconAddNewButton(Icons.rss_feed, "RSS", () {}),
+                        _iconAddNewButton(Icons.rss_feed, "ATOM", () {}),
+                      ],
+                    ),
+                    Container(
+                      width: 34.0,
+                      height: 34.0,
+                      child: FloatingActionButton(
+                        child: Icon(Icons.close),
+                        foregroundColor: Theme.of(context).primaryColor,
+                        backgroundColor:
+                            Theme.of(context).textTheme.caption.color,
+                        elevation: 0.0,
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
         },
+      ),
+    );
+  }
+
+  Widget _iconAddNewButton(
+      IconData icon, String title, VoidCallback onPressed) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        children: <Widget>[
+          FloatingActionButton(
+            elevation: 1.0,
+            highlightElevation: 8.0,
+            onPressed: onPressed,
+            child: Icon(icon),
+          ),
+          GestureDetector(
+            onTap: onPressed,
+            child: Padding(
+              padding: EdgeInsets.all(12.0),
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: 11.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
